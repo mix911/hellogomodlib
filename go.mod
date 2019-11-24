03943ba551c2fe66f28c0e79d6d22e1c7937b5dd
mix911 <wmihanale@gmail.com>
2019-11-24 17:30:47 +0300

    Multilanguage was added

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/mix911/hellogomodlib
+module github.com/mix911/hellogomodlib/v2
 
 go 1.13
--- testmod.go
+++ testmod.go
@@ -1,8 +1,16 @@
 package testmod
 
 import "fmt"
+import "errors"
 
 // Hi ...
-func Hi(name string) string {
-	return fmt.Sprintf("Hi, %s!", name)
+func Hi(name string, lang string) (string, error) {
+	switch lang {
+	case "en":
+		return fmt.Sprintf("Hello, %s", name), nil
+	case "ru":
+		return fmt.Sprintf("Привет, %s", name), nil
+	default:
+		return "", errors.New("Unknown language")
+	}
 }
